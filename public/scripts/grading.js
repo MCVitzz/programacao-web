4e5ce7b078116b0216f13f7cafe9086552f74ebc
Vasco Pereira <vascop_98@hotmail.com>
2020-11-16 15:59:50 +0000

    Aula 3

--- public/scripts/grading.js
+++ public/scripts/grading.js
@@ -1,36 +1,61 @@
+const students = [
+    {
+        name: 'Francis Lamb',
+        number: 50037624
+    },
+    {
+        name: 'Hugh Pole',
+        number: 50037566
+    },
+    {
+        name: 'Dwight Saints',
+        number: 50037782
+    },
+    {
+        name: 'Andy Passion',
+        number: 50037236
+    },
+    {
+        name: 'Charles Martins',
+        number: 50037128
+    },
+];
+
+const units = [
+    {
+        name: 'Mathematics',
+        semester: '3rd Semester',
+        ects: 6
+    },
+    {
+        name: 'Literature',
+        semester: '2nd Semester',
+        ects: 6
+    },
+    {
+        name: 'Laws',
+        semester: '1st Semester',
+        ects: 3
+    },
+    {
+        name: 'Informatics',
+        semester: '1st Semester',
+        ects: 6
+    },
+    {
+        name: 'Cooking',
+        semester: '2nd Semester',
+        ects: 3
+    },
+];
+
+
 window.onload = () => {
 
+    addOptions();
+
     document.getElementById('calculate').onclick = () => {
-        var output = document.getElementById('output');
-
-        var studentName = document.getElementById('studentName').value;
-
-        var unitName = document.getElementById('unitName').value;
-
-        var projectGrade = document.getElementById('projectGrade').value;
-        var projectPercentage = document.getElementById('projectPercentage').value;
-
-        var testGrade = document.getElementById('testGrade').value;
-        var testPercentage = document.getElementById('testPercentage').value;
-
-        var fields = document.querySelectorAll('input[type=text]');
-
-        for (field of fields) {
-            if (field.value == '') {
-                alert('Please fill out all fields.');
-                return;
-            }
-        }
-
-        if (testPercentage + projectPercentage != 100) {
-            alert('The percentages don\'t add up to 100.');
-            return;
-        }
-
-        var finalGrade = projectGrade * projectPercentage * 0.01 + testGrade * testPercentage * 0.01;
-        Math.ceil(finalGrade);
-
-        output.innerHTML = `Student ${studentName} obtained ${finalGrade} on the ${unitName} unit.`;
+        calculate();
     };
 
     document.getElementById('reset').onclick = () => {
@@ -40,3 +65,56 @@ window.onload = () => {
         }
     };
 };
+
+function addOptions() {
+    let selectStudent = document.getElementById('studentName');
+    let selectUnit = document.getElementById('unitName');
+
+    selectStudent.innerHTML += `<option value="">Select..</option>`;
+    selectUnit.innerHTML += `<option value="">Select..</option>`;
+
+    for (let idx in students) {
+        selectStudent.innerHTML += `<option value="${idx}">${students[idx].name}</option>`;
+    }
+    for (let idx in units) {
+        selectUnit.innerHTML += `<option value="${idx}">${units[idx].name}</option>`;
+    }
+}
+
+function calculate() {
+
+    var output = document.getElementById('output');
+
+    var studentIdx = document.getElementById('studentName').value;
+
+    var unitIdx = document.getElementById('unitName').value;
+
+    var projectGrade = parseFloat(document.getElementById('projectGrade').value);
+    var projectPercentage = parseFloat(document.getElementById('projectPercentage').value);
+
+    var testGrade = parseFloat(document.getElementById('testGrade').value);
+    var testPercentage = parseFloat(document.getElementById('testPercentage').value);
+
+    var fields = document.querySelectorAll('input[type=text], select');
+
+    for (field of fields) {
+        if (field.value == '') {
+            alert('Please fill out all fields.');
+            return;
+        }
+    }
+
+    if (testPercentage + projectPercentage != 100) {
+        alert('The percentages don\'t add up to 100.');
+        return;
+    }
+
+    var finalGrade = projectGrade * projectPercentage * 0.01 + testGrade * testPercentage * 0.01;
+    Math.ceil(finalGrade);
+
+    let student = students[studentIdx];
+    let unit = units[unitIdx];
+
+
+    output.innerHTML = `Student ${student.name} with the number ${student.number} obtained ${finalGrade.toFixed(1)} on the ${unit.name} unit (${unit.ects} ECTS) of the ${unit.semester}.`;
+}
--- public/scripts/studentGrades.js
+++ public/scripts/studentGrades.js
@@ -1,21 +1,50 @@
-var student;
+const studentName = 'Francis Lamb';
 
-var unitGrades = [];
+const unitGrades = [
+    {
+        name: 'Mathematics',
+        grade: 8.3,
+        semester: '3rd Semester',
+        ects: 6
+    },
+    {
+        name: 'Literature',
+        grade: 11.2,
+        semester: '2nd Semester',
+        ects: 6
+    },
+    {
+        name: 'Laws',
+        grade: 18.5,
+        semester: '1st Semester',
+        ects: 3
+    },
+    {
+        name: 'Informatics',
+        grade: 14.3,
+        semester: '1st Semester',
+        ects: 6
+    },
+    {
+        name: 'Cooking',
+        grade: 7.4,
+        semester: '2nd Semester',
+        ects: 3
+    },
+];
 
 window.onload = () => {
-    student = 'Francis Lamb';
 
-    setStudentTag(`${student} grades`);
+    let { unitsFailed, gradeSum, ectsSum } = getCalcs();
 
-    unitGrades.push({
-        unit: 'Mathematics',
-        grade: 7.5,
-    });
+    let unitsFinished = unitGrades.length - unitsFailed;
 
-    unitGrades.push({
-        unit: 'Literature',
-        grade: 11.2,
-    });
+    let average = gradeSum / ectsSum;
+
+    setSummaryValues(unitsFailed, unitsFinished, average);
+
+
+    setStudentTag(`${studentName} grades`);
 
     showCards();
 };
@@ -25,22 +54,55 @@ function setStudentTag(text) {
 }
 
 function showCards() {
-    for (var grade of unitGrades) {
-        var card = buildCard(grade);
+    for (let grade of unitGrades) {
+        let card = buildCard(grade);
         document.getElementById('container').innerHTML += card;
     }
 }
 
-function buildCard(grade) {
-    var html = `<div class="card ${(grade.grade <= 9.5) ? 'failed' : ''}">`;
+function getCalcs() {
+    let gradeSum = 0;
+    let ectsSum = 0;
+    let unitsFailed = 0;
+    for (let unit of unitGrades) {
+        if (unit.grade < 9.5) {
+            unitsFailed++;
+        }
+        gradeSum += unit.grade * unit.ects;
+        ectsSum += unit.ects;
+    }
+    return {
+        'gradeSum': gradeSum,
+        'ectsSum': ectsSum,
+        'unitsFailed': unitsFailed
+    };
+}
+
+function setSummaryValues(passed, failed, average) {
+    setAverageTag(average.toFixed(1));
+    let element = document.getElementById('summary');
+    element.innerHTML += getParagraph(`${failed} failed units`);
+    element.innerHTML += getParagraph(`${passed} passed units`);
+}
+
+function getParagraph(text) {
+    return `<p>${text}</p>`;
+}
+
+function setAverageTag(average) {
+    document.getElementById('summaryText').innerHTML = average;
+}
+
+function buildCard(unit) {
+    var html = `<div class="card ${(unit.grade <= 9.5) ? 'failed' : ''}">`;
     html += '<section class="card-grade-title ">';
     html += '<div class="card-head">';
-    html += `<h1>${grade.unit.substring(0, 2)}</h1>`;
+    html += `<h1>${unit.name.substring(0, 2)}</h1>`;
     html += '</div>';
-    html += `<h4>${grade.unit}</h4>`;
+    html += `<h4>${unit.name}</h4>`;
     html += '<div>';
     html += '</section>';
-    html += `<p>Grade: ${grade.grade}</p>`
+    html += `<p>Grade: ${unit.grade}</p>`
     html += '</div>';
     return html;
 }
